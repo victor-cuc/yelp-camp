9eba51735f551555f89bc8f1ecb7f0e9a9d8a5d4
Victor Cuc <victorcuc99@yahoo.com>
2019-02-18 18:20:08 +0000

    First pages and routes

--- app.js
+++ app.js
@@ -5,7 +5,29 @@ app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   res.render("landing")
-})
+});
+
+app.get('/campgrounds', (req, res) => {
+  
+  var campgrounds = [
+    {
+      name: "Salmon Creek", 
+      image: "https://s3.amazonaws.com/imagescloud/images/medias/reservation/camping/main.jpg"
+    },
+    {
+      name: "La mici", 
+      image: "http://www.hqshuibiao.com/wp-content/uploads/2018/10/general-.jpg"
+    },
+    {
+      name: "La munte la mici", 
+      image:"https://d2ciprw05cjhos.cloudfront.net/files/v3/styles/gs_large/public/images/18/06/gettyimages-649155058.jpg?itok=Lhx5ciAR"
+    },
+  ];
+
+  res.render('campgrounds', {campgrounds: campgrounds});
+
+});
+
 app.listen('3000', () => {
   console.log("Started YelpCamp server");
 });
