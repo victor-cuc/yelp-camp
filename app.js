d9cbf130603d20f83fc0dd47465d0860b347a0fa
Victor Cuc <victorcuc99@yahoo.com>
2019-02-19 18:18:26 +0000

    Added database functionality

--- app.js
+++ app.js
@@ -1,62 +1,98 @@
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
+const express = require('express'),
+      app = express(), 
+      bodyParser = require('body-parser'), 
+      mongoose = require("mongoose");
 
+mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true });
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 
+const campgroundSchema = new mongoose.Schema({
+  name: String,
+  image: String
+});
+
+const Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create(
+//   {
+//     name: "La mici", 
+//     image: "http://www.hqshuibiao.com/wp-content/uploads/2018/10/general-.jpg"
+//   }, 
+//   (err, campground) => {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       console.log("new campground " + campground.name);
+//     }
+//   }
+// );
+
 app.get("/", (req, res) => {
-  res.render("landing")
+  res.render("landing");
 });
 
-var campgrounds = [
-  {
-    name: "Salmon Creek", 
-    image: "https://s3.amazonaws.com/imagescloud/images/medias/reservation/camping/main.jpg"
-  },
-  {
-    name: "La mici", 
-    image: "http://www.hqshuibiao.com/wp-content/uploads/2018/10/general-.jpg"
-  },
-  {
-    name: "La munte la mici", 
-    image:"https://d2ciprw05cjhos.cloudfront.net/files/v3/styles/gs_large/public/images/18/06/gettyimages-649155058.jpg?itok=Lhx5ciAR"
-  },
-  {
-    name: "Salmon Creek", 
-    image: "https://s3.amazonaws.com/imagescloud/images/medias/reservation/camping/main.jpg"
-  },
-  {
-    name: "La mici", 
-    image: "http://www.hqshuibiao.com/wp-content/uploads/2018/10/general-.jpg"
-  },
-  {
-    name: "La munte la mici", 
-    image:"https://d2ciprw05cjhos.cloudfront.net/files/v3/styles/gs_large/public/images/18/06/gettyimages-649155058.jpg?itok=Lhx5ciAR"
-  },
-];
+// var campgrounds = [
+//   {
+//     name: "Salmon Creek", 
+//     image: "https://s3.amazonaws.com/imagescloud/images/medias/reservation/camping/main.jpg"
+//   },
+//   {
+//     name: "La mici", 
+//     image: "http://www.hqshuibiao.com/wp-content/uploads/2018/10/general-.jpg"
+//   },
+//   {
+//     name: "La munte la mici", 
+//     image:"https://d2ciprw05cjhos.cloudfront.net/files/v3/styles/gs_large/public/images/18/06/gettyimages-649155058.jpg?itok=Lhx5ciAR"
+//   },
+//   {
+//     name: "Salmon Creek", 
+//     image: "https://s3.amazonaws.com/imagescloud/images/medias/reservation/camping/main.jpg"
+//   },
+//   {
+//     name: "La mici", 
+//     image: "http://www.hqshuibiao.com/wp-content/uploads/2018/10/general-.jpg"
+//   },
+//   {
+//     name: "La munte la mici", 
+//     image:"https://d2ciprw05cjhos.cloudfront.net/files/v3/styles/gs_large/public/images/18/06/gettyimages-649155058.jpg?itok=Lhx5ciAR"
+//   },
+// ];
 
+//INDEX - show all campgrounds
 app.get('/campgrounds', (req, res) => {
-  res.render('campgrounds', {campgrounds: campgrounds});
+  // res.render('campgrounds', {campgrounds: campgrounds});
+  Campground.find({}, (err, storedCampgrounds) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render('campgrounds', {campgrounds: storedCampgrounds})
+    }
+  });
 });
 
+//CREATE - add new campground to db
 app.post("/campgrounds", (req, res) => {
-  res.send("You hit the post route");
-  
   //get data from form and redirect to camgrounds array
   const name = req.body.name;
   const img = req.body.image;
   const newCampground = {name: name, image: img};
 
-  //redirect back to /campgrounds
-  campgrounds.push(newCampground);
-
+  Campground.create(newCampground, (err, campground) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Added: " + campground.name);
+      res.redirect("/campgrounds");
+    }
+  });
 });
 
+//NEW - show form to create new campgrounds
 app.get("/campgrounds/new", (req, res) => {
   res.render("new");
 });
 
-app.listen('3000', () => {
+app.listen(3000, () => {
   console.log("Started YelpCamp server");
 });
